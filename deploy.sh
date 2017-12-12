33f6a2bfb5b1975d3494b8289bdf4b4d5483be73
Jason Horn <jason.horn@clearobject.com>
2017-12-12 15:52:48 -0500

    Updating the public key for cloudadmin

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 adduser -g wheel cloudadmin
 mkdir /home/cloudadmin/.ssh
-echo "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA2d99zK9WsoVUZYRpphGqDaaij0aJLu1446u45UKnQasacJIZbJTxgxwVP0xj3wHb3qs+VG6IqT95PgNXE2CVPYE1Xceht0HOOc0PMDbwLxYrd2Y459lT7Wikvr3p0xW1oJANOy15Qipw8UDph2HlWWJQx3qMyihkOiaribv4uh0547hvuW4Drt+TG6DIEgHBOvqZNvyOBGdDuQ2jkppq2AUGW7CpPxjBo+ZTE9BCw9GqPGUAyrMMiViOg0SMGwtg651BlRsKTeX3t0Gh2/kUr+uiaPgAkc3mggDAkOSZhzuuZOgWUBFcgBYVn0F3Laz1cDT3/71aRVrqku2yAT004w==" > /home/cloudadmin/.ssh/authorized_keys
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6sMBWqeONW+0kEtXEmi3BdxKxu0R+kNVZf4xsjgWqigWvr//02DNw8zrV34uljT9KACy0ZpMtyyABUZ/pZOVT4g4HU5ONIMqpxxTrHXIx/+dy3TF544XdFXUImfzUS5IIPZaDf3MU+r3X4fHiuTK1y9PbR6hYJIUMZHO3hvIy7PPrKZwx4r+BlUEP8BYn5Sl9FkMNdFd+WJmyZ8svD+fRNIOZm+2U+1uyMLLgCZxEYCy0NP19pZijNovLtt2v27LV6q1WNPjaz5/UZAHbHF70fZMsAQaUcHs0xBGSc6rJrXsbpsoC1kY8fUpu4VIvQzn5mIuSf/a2F/l5FbruplkfQ==" > /home/cloudadmin/.ssh/authorized_keys
 chown -R cloudadmin:wheel /home/cloudadmin/
 chmod 0700 /home/cloudadmin/.ssh
 chmod 0600 /home/cloudadmin/.ssh/authorized_keys
